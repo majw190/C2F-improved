7907cb4cfc7c35c8d81278b0fad3d8a6400a3030
thenextprevious <76392789+majw190@users.noreply.github.com>
2025-03-03 22:31:28 +0000

    Create C2F.c

---
+++ C2F.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+     const float FAHRENHEIT_SCALE = 9.0 / 5.0;
+     // offset between two scales is 32
+     const float FAHRENHEIT_OFFSET = 32.0;
+
+     float celsius, fahrenheit;
+     int lower, upper, step;
