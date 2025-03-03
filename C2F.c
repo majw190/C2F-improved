be27afa9d2e5bf106ed7e62f3f3306aeb07e2a13
thenextprevious <76392789+majw190@users.noreply.github.com>
2025-03-03 22:37:27 +0000

    Update C2F.c

--- C2F.c
+++ C2F.c
@@ -8,11 +8,11 @@ int main()
 
     if (lower > upper || step <= 0) 
     {
-        printf("Invalid input: Ensure lower < upper and step > 0.\n");
+        printf("Invalid: The temperature you are submitting must be less than the ending temperature, and positive");
         return 1;
     }
 
-    printf("\nCelsius\tFahrenheit\n-------\t----------\n");
+    printf("\nTemperature in Celsius\tTemperature in Fahrenheit\n-----------------------\t-------------------------\n");
     for (float celsius = lower; celsius <= upper; celsius += step)
         printf("%6.1f\t%10.1f\n", celsius, (9.0 / 5.0 * celsius) + 32);
 
